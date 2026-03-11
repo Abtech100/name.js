3561c6d78c57b6e903779a687fbfbd5c24e14788
Abiodun Agbaji (ABTECH) <abiodune752@gmail.com>
2026-03-11 22:51:03 +0100

    my info program

--- intro.js
+++ intro.js
@@ -1,3 +1,7 @@
-const name = 'Abiodun'
+const name = 'Agbaji Abiodun'
+const age = 21
+const favoriteLanguage = 'JavaScript'
 
-console.log(name)
+console.log("Name:", name)
+console.log("Age:", age)
+console.log("Favorite Language:", favoriteLanguage)
